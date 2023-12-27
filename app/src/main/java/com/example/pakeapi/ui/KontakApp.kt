4cc588c933571e4c35d8fb487362f01e912a01b8
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-27 14:07:16 +0700

    menambahkan code untuk menampilkan button NavigateBack dengan icon panah untuk kembali ke halaman sebelumnya

--- app/src/main/java/com/example/pakeapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/pakeapi/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.pakeapi.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -58,7 +62,16 @@ fun TopAppBarKontak(
         title = { Text(title) },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
-        navigationIcon = {}
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        }
     )
 }
 

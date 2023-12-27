a6decb7818b2d5f0b005ca37af8c9b7d8379ca16
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-27 15:28:27 +0700

    menambahkan Scaffold untuk menyusun struktur dasar layar

--- app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -171,4 +172,6 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold() {}
 }

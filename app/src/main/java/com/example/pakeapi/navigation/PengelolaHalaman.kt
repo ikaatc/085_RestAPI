0ccb8e02b7e120496a6b521c0b018f4fb39234db
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-27 21:57:31 +0700

    membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/pakeapi/navigation/PengelolaHalaman.kt
@@ -0,0 +1,7 @@
+package com.example.pakeapi.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {}

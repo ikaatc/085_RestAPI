997b4ec82179b7198fd40d0a647d12fdc3e4378b
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-27 14:36:16 +0700

    menambahkan onDetailClick dan onDeleteClick

--- app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
@@ -45,7 +45,13 @@ fun HomeStatus(
         is KontakUIState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
         is KontakUIState.Success -> KontakLayout(
             kontak = kontakUIState.kontak,
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier.fillMaxWidth(),
+            onDetailClick = {
+                onDetailClick(it.id)
+            },
+            onDeleteClick = {
+                onDeleteClick(it)
+            }
         )
         is KontakUIState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }

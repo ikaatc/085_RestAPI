a435dcb1e6825c92bcfc674878f12efbd3812218
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-27 21:58:33 +0700

    menambahkan NavHost

--- app/src/main/java/com/example/pakeapi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pakeapi/navigation/PengelolaHalaman.kt
@@ -1,7 +1,16 @@
 package com.example.pakeapi.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import com.example.pakeapi.ui.home.screen.DestinasiHome
 
 @Composable
-fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {}
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {}
+}

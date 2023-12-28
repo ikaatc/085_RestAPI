0fbef154444b613071cbfbc734816af99efeb870
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-28 19:00:47 +0700

    menambah code kekurangan

--- app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pakeapi/ui/home/screen/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -148,16 +149,26 @@ fun KontakCard(
                         contentDescription = null,
                     )
                 }
-
                 Text(
                     text = kontak.nohp,
                     style = MaterialTheme.typography.titleMedium,
                 )
             }
-            Text(
-                text = kontak.alamat,
-                style = MaterialTheme.typography.titleMedium,
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = kontak.alamat,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
+            }
         }
     }
 }
